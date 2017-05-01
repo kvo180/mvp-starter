17d9519294180653ca521bdd363f4270fa841d3f
kvo180 <khoavo180@gmail.com>
2017-05-01 00:14:00 -0700

    Hide NEOList table if no NEOs are added yet

--- react-client/src/components/NEOList.jsx
+++ react-client/src/components/NEOList.jsx
@@ -11,21 +11,22 @@ const NEOList = (props) => {
     <h4> Near Earth Objects </h4>
     There are { props.neos.length } near earth objects.
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th id="nameTitle">Name</th>
-            <th id="sizeTitle">Diameter (mi)</th>
-            <th id="velocityTitle">Relative Velocity (mi/h)</th>
-            <th id="approachDate">Approach Date</th>
-            <th id="missDistance">Miss Distance (mi)</th>
-            <th id="hazardous">Potentially Hazardous</th>
-          </tr>
-        </thead>
-        <tbody>
-          {neosArray}
-        </tbody>
-      </table>
+      {props.neos.length > 0 ?
+        <table>
+          <thead>
+            <tr>
+              <th id="nameTitle">Name</th>
+              <th id="sizeTitle">Diameter (mi)</th>
+              <th id="velocityTitle">Relative Velocity (mi/h)</th>
+              <th id="approachDate">Approach Date</th>
+              <th id="missDistance">Miss Distance (mi)</th>
+              <th id="hazardous">Potentially Hazardous</th>
+            </tr>
+          </thead>
+          <tbody>
+            {neosArray}
+          </tbody>
+        </table> : null}
     </div>
   </div>
   );
